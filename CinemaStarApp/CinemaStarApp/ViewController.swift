1c9afe1b44f00732e7473b98ea2f248ee4993a17
Suprunov Anton <suprunovvanton666@gmail.com>
2024-04-22 14:10:25 +0300

    Приложение SinemaStar

---
+++ CinemaStarApp/CinemaStarApp/ViewController.swift
@@ -0,0 +1,12 @@
+// ViewController.swift
+// Copyright © RoadMap. All rights reserved.
+
+import UIKit
+
+///
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("")
+    }
+}
